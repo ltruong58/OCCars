51776a774927fa7ba5f9bf5c9f47d0a2627fcb7b
ltruong58 <long.truongnguyenthanh@gmail.com>
2016-09-15 16:27:14 -0700

    2

---
+++ app/src/main/java/com/example/longtruong/cs273/ltruong58/occars/Car.java
@@ -0,0 +1,79 @@
+package com.example.longtruong.cs273.ltruong58.occars;
+
+/**
+ * Created by ltruong58 on 9/15/2016.
+ */
+public class Car {
+    private double mPrice;
+    private double mDownPayment;
+    private int mLoanTerm;
+    private static final  double TAX_RATE = 0.08;
+    private static final  double THREE_YEARS_INTEREST_RATE = 0.0462;
+    private static final  double FOUR_YEARS_INTEREST_RATE = 0.0416;
+    private static final  double FIVE_YEARS_INTEREST_RATE = 0.0419;
+
+    public Car() {
+        mPrice = 0.0;
+        mDownPayment = 0.0;
+        mLoanTerm = 3;
+    }
+
+    public double getPrice() {
+        return mPrice;
+    }
+
+    public void setPrice(double mPrice) {
+        this.mPrice = mPrice;
+    }
+
+    public double getDownPayment() {
+        return mDownPayment;
+    }
+
+    public void setDownPayment(double mDownPayment) {
+        this.mDownPayment = mDownPayment;
+    }
+
+    public int getLoanTerm() {
+        return mLoanTerm;
+    }
+
+    public void setLoanTerm(int mLoanTerm) {
+        this.mLoanTerm = mLoanTerm;
+    }
+
+    public String monthlyPayment()
+    {
+        String s = "";
+        return s;
+    }
+    public double calculateBorrowedAmount()
+    {
+        return mPrice - mDownPayment + calculateTaxAmount();
+    }
+    public double calculateInterestAmount()
+    {
+        double borrowedAmount;
+        borrowedAmount = calculateBorrowedAmount();
+        if(mLoanTerm == 3)
+            return borrowedAmount * THREE_YEARS_INTEREST_RATE;
+        else if(mLoanTerm == 4)
+            return borrowedAmount * FOUR_YEARS_INTEREST_RATE;
+        else
+            return borrowedAmount * FIVE_YEARS_INTEREST_RATE;
+    }
+    public double calculateMonthlyPayment()
+    {
+        return (calculateBorrowedAmount() + calculateInterestAmount()) / (12 * mLoanTerm);
+    }
+    public double calculateTaxAmount()
+    {
+        return mPrice * TAX_RATE;
+    }
+
+    public double calculateTotalAmount()
+    {
+        return mPrice + calculateTaxAmount();
+    }
+
+}
--- app/src/main/java/com/example/longtruong/cs273/ltruong58/occars/PurchaseActivity.java
+++ app/src/main/java/com/example/longtruong/cs273/ltruong58/occars/PurchaseActivity.java
@@ -1,13 +1,76 @@
 package com.example.longtruong.cs273.ltruong58.occars;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class PurchaseActivity extends AppCompatActivity {
 
+    private EditText carPriceEditText;
+    private EditText downPaymentEditText;
+    private RadioButton threeYearsRadioButton;
+    private RadioButton fourYearsRadioButton;
+    private RadioButton fiveYearsRadioButton;
+
+    private Car currentCar;
+
+    private String monthlyPaymentText, loanSummaryText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase);
+
+        carPriceEditText = (EditText) findViewById(R.id.priceEditText);
+        downPaymentEditText = (EditText) findViewById(R.id.paymentEditText);
+        threeYearsRadioButton = (RadioButton) findViewById(R.id.threeYearsRadioButton);
+        threeYearsRadioButton = (RadioButton) findViewById(R.id.threeYearsRadioButton);
+        threeYearsRadioButton = (RadioButton) findViewById(R.id.threeYearsRadioButton);
+
+        currentCar = new Car();
+    }
+
+    // When associating the button with an event, set the onClock property
+    // Define the method as public void (with one parameter View view)
+    public void activateLoanReport(View view)
+    {
+        double price, downPayment;
+
+        try {
+            price = Double.parseDouble(carPriceEditText.getText().toString());
+            downPayment = Double.parseDouble(downPaymentEditText.getText().toString());
+        }
+        catch(NumberFormatException e) {
+            price = 0.0;
+            downPayment = 0.0;
+        }
+
+        int loanTerm;
+
+        if(fiveYearsRadioButton.isChecked())
+            loanTerm = 5;
+        else if (fourYearsRadioButton.isChecked())
+            loanTerm = 4;
+        else
+            loanTerm = 3;
+
+        currentCar.setPrice(price);
+        currentCar.setDownPayment(downPayment);
+        currentCar.setLoanTerm(loanTerm);
+
+        // Create the Intent to share data with LoanSummaryActivity
+        Intent loanSummaryIntent = new Intent(this, LoanSummaryActivity.class);
+        loanSummaryIntent.putExtra("MonthlyPayment", monthlyPaymentText);
+        loanSummaryIntent.putExtra("LoanSummary", loanSummaryText);
+
+        // Start the new activity with the intent data
+        startActivity(loanSummaryIntent);
+    }
+
+    private void constructLoanSummaryText(){
+        monthlyPaymentText = getString(R.string.report_line1) + currentCar.monthlyPayment();
+        loanSummaryText = getString(R.string.report_line2) + currentCar.getPrice();
     }
 }
